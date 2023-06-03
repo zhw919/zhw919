29a2ad79ec9d6cbc0a30bc99784dadb4350792d8
zhw <453754765@qq.com>
2023-06-03 10:24:19 +0800

    Revert "第一次练习"
    This reverts commit 34c394ffd52b21c5dc7471817d99b9ba98aad318.

--- src/main/java/demo/mapper/StuMapper.java
+++
@@ -1,9 +0,0 @@
-package demo.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import demo.domain.Student;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface StuMapper extends BaseMapper<Student> {
-}
